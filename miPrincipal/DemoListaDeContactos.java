8c3f3ff7f4ba5cdc414fa3280cb35d7222fa33a9
Alonso Castro Vega <94109579+CastroVegaAlonso@users.noreply.github.com>
2024-04-24 16:08:16 +0000

    Actualizado

--- miPrincipal/DemoListaDeContactos.java
+++ miPrincipal/DemoListaDeContactos.java
@@ -9,10 +9,21 @@ public class DemoListaDeContactos {
         System.err.println();
         ListaDeContactos ldc = new ListaDeContactos();
         //agregamos algunos contactos
-        ldc.agregarContactos("Fernando", "Castro", "C11 No 12 Culiacan","fernandocastro@gmail.com", "6677160732", "6677511511");
-        ldc.agregarContactos("Maria Andrea","Vargas"," C11 No 705 Culiacan", "mariv@gmail.com","6677121429","6677306142");
-        ldc.agregarContactos("Carolina", "Sanchez",",","carolsa@gmail.com", "6677121212","6677982515");
-        ldc.agregarContactos("Federica", "Cardenas", ",", "fede@gmail.com","6677161111","6677921456");
+        ldc.agregarContactos("Fernando", "Castro", 
+           "C11 No 12 Culiacan","fernandocastro@gmail.com", 
+           "6677160732", "6677511511");
+
+        ldc.agregarContactos("Maria Andrea","Vargas",
+        " C11 No 705 Culiacan", "mariv@gmail.com",
+        "6677121429","6677306142");
+
+        ldc.agregarContactos("Carolina", "Sanchez",
+        ",","carolsa@gmail.com", 
+        "6677121212","6677982515");
+
+        ldc.agregarContactos("Federica", "Cardenas", 
+        ",", "fede@gmail.com",
+        "6677161111","6677921456");
 
         Lista<Contacto> listado = ldc.mostrarTodosLosContactos();
         System.out.println("--- Listado después de la agregación ---");
@@ -28,7 +39,8 @@ public class DemoListaDeContactos {
         for(int i=0;i<listado.getTamanio();i++){
             System.out.println(listado.getValor(i));
         }
-        ldc.modificarContacto("Federica", "Cardenas", "laguna de viezca 705 culiacan", "fede@gmail.com", "6677161111", "6677921456");
+        ldc.modificarContacto("Federica","Cardenas","laguna de viezca 705 culiacan",
+        "fede@gmail.com","6677161111","6677921456");
         
         listado = ldc.mostrarTodosLosContactos();
         System.out.println("--- Listado después de la modificación ---");
